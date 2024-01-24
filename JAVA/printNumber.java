2e78a86b73eb3acf08d8990e91cb57f0061aa7d6
Saidur KALI <saidurrahman@iut-dhaka.edu>
2024-01-25 01:26:56 +0600

    learning java

---
+++ JAVA/printNumber.java
@@ -0,0 +1,13 @@
+package JAVA;
+
+public class printNumber {
+   public static void main(String[] args){
+    int a =5;
+    int b =6;
+    System.out.println("The value of a is:" + a +"  .");
+    System.out.println("The value of b is:" + b +"  .");
+    System.out.println("Sum is:"+ a+b); // not right way
+    System.out.println("Sum is:"+ (a+b));
+
+   }
+}
