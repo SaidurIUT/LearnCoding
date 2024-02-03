99b1ce6104bc030503d54b7dab542d30fb4eaaf3
Saidur KALI <saidurrahman@iut-dhaka.edu>
2024-02-03 21:24:07 +0600

    learned cpp pair

---
+++ CPP/pair.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    pair<int,string> p;
+
+    p.first=1;
+    p.second= "Sagor";
+
+    cout<<p.first<<" "<<p.second<<endl;
+}
